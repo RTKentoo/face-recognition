732b6fa7622a392abea51a1fff9960adaf2ed60f
rkenttaylor <r.t.kent.oo@gmail.com>
2019-08-28 10:08:42 -0700

    update logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import tachyons from 'tachyons'
 
 
@@ -7,9 +9,9 @@ function App() {
   return (
     <div>
       <Navigation />
-      {/**<Logo />
+      <Logo /> 
       <ImageLinkForm />
-      <FaceRecognition />**/}
+      {/**<FaceRecognition />**/}
     </div>
   )
 }
